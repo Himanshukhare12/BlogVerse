9fe40d39938d407aa750f47f7cd3c5dd1a5ebaa2
Himanshu khare <hkhare666@gmail.com>
2025-07-08 21:27:26 +0530

    Add some more components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,12 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-
-
-
-export {Header,Footer}
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from './Header/LogoutBtn'
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
