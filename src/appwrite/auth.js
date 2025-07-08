1a2e2c2c953a5fd51ba71d72bec5410049a08b66
Himanshu khare <hkhare666@gmail.com>
2025-07-08 12:02:44 +0530

    setup store and some components

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -2,16 +2,18 @@ import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService{
-    client=new Client;
+    client=new Client();
     account;
     constructor()
     {
-        this.client().setProject(conf.appwriteProjectId)
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
         this.account=new Account(this.client);
     }
     async createAccount({email,password,name}){
         try {
-        const userAccount=await this.account.create(ID.unique,email,password,name)
+        const userAccount=await this.account.create(ID.unique(),email,password,name)
         if(userAccount)
         {
             return this.login({email,password});
@@ -34,9 +36,9 @@ export class AuthService{
         try {
             return await this.account.get();
         } catch (error) {
-            throw error;
+            console.log("Appwrite service :: getCurrentUser :: error",error);
+            return null
         }
-        return null;
     }
     async logout(){
         try {
