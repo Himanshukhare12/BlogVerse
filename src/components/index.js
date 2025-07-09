5c8d41b8aa9915fae6523d827d4fd588c434971c
Himanshu khare <hkhare666@gmail.com>
2025-07-09 18:59:27 +0530

    Add some more component

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,13 @@ import Header from "./Header/Header";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from './Header/LogoutBtn'
+import RTE from "./RTE";
 export {
     Header,
     Footer,
-    Container,
     Logo,
-    LogoutBtn
+    Container,
+    LogoutBtn,
+    RTE
+    
 }
